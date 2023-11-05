ad1bc3bf3d29581bd13700a00d0fb3305c3825af
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-11-05 00:54:28 -0300

    Score board

--- game/__init__.py
+++ game/__init__.py
@@ -1,7 +1,7 @@
-import pandas as pd
 from os import system
 
 from field import Field
+from score import Score
 
 ships = {
     5 : {'C' : 'carrier'},
@@ -11,16 +11,10 @@ ships = {
     1 : {'' : ''}
 }
 
-columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-lines = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
-table = pd.DataFrame(data = '+', index = lines, columns = columns)
-
 system('cls')
-#print(table)
-
-#msg = '\nAguarde...\n'
-#print(f'\033[33m{msg}\033[m')
 
 field = Field()
 field.render()
+
+score = Score()
+score.render()
--- game/score.py
+++ game/score.py
@@ -1,6 +1,7 @@
 class Score():
     '''Create score board.'''
     def __init__(self, player_name:str = 'Player') -> None:
+        self.total_hits = 35 # (1 * 5) + (2 * 4) + (3 * 3) + (4 * 2) + (5 * 1)
         self.player_name = player_name
         self.bot_name = 'Bot'
         self.player_life = 100
@@ -8,7 +9,41 @@ class Score():
         self.player_score = 0
         self.bot_score = 0
         self.turn = self.player_name
+        self.text_color = '\033[33m'
+        self.end_color = '\033[m'
+
+    def change_turn(self) -> None:
+        '''Change turn.'''
+        if self.turn == self.player_name:
+            self.turn = self.bot_name
+        else:
+            self.turn = self.player_name
+
+    def hit(self, target:str = 'Bot') -> None:
+        '''Ship hit.'''
+        hit = 100 / self.total_hits
+        if target == self.player_name:
+            self.player_life -= hit
+            if self.player_life <= 0:
+                self.player_life = 0
+                self.bot_score += 1
+        else:
+            self.bot_life -= hit
+            if self.bot_life <= 0:
+                self.bot_life = 0
+                self.player_score += 1
+
 
     def render(self) -> None:
         '''Render score board.'''
-        pass
+        print(self.text_color + '=' * 32)
+        turn = f'É a vez de {self.turn}...'
+        print(f'{turn:^32}')
+        print(self.text_color + '=' * 32)
+        player_name = f'Nome: {self.player_name}'
+        print(f'{player_name:<15} | Nome: {self.bot_name}')
+        player_life = f'Vida: {int(self.player_life)} %'
+        print(f'{player_life:<15} | Vida: {int(self.bot_life)} %')
+        player_score = f'Pontos: {self.player_score}'
+        print(f'{player_score:<15} | Pontos: {self.bot_score}')
+        print('=' * 32 + self.end_color)
