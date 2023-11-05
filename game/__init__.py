e7f0bee6cbb78927d5066853fbe51f72ed20651d
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-11-04 22:42:44 -0300

    Field and render field

--- game/__init__.py
+++ game/__init__.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from os import system
 
+from models import Field
+
 ships = {
     5 : {'C' : 'carrier'},
     4 : {'B' : 'battleship'},
@@ -12,29 +14,13 @@ ships = {
 columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 lines = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-for i, col in enumerate(columns):
-    columns[i] = '\033[33m' + col + '\033[m'
-for i, line in enumerate(lines):
-    lines[i] = '\033[33m' + line + '\033[m'
-
-field = [
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', '']
-]
-
-for i in range(10):
-    for j in range(10):
-        field[i][j] = '\033[34mX\033[m'
-
-table = pd.DataFrame(data = field, index = lines, columns = columns)
+table = pd.DataFrame(data = '+', index = lines, columns = columns)
 
 system('cls')
-print(table)
+#print(table)
+
+#msg = '\nAguarde...\n'
+#print(f'\033[33m{msg}\033[m')
+
+field = Field()
+field.render_field()
---
+++ game/models.py
@@ -0,0 +1,40 @@
+class Field():
+    '''Create game field.'''
+    def __init__(self, col_space:int = 2) -> None:
+        self.col_space = col_space
+        self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        self.lines = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        self.bg_char = '+'
+        self.bg_color = '\033[34m'
+        self.text_color = '\033[33m'
+        self.end_color = '\033[m'
+        self.table = self.create_table()
+
+    def create_table(self) -> list:
+        '''Create table.'''
+        table = []
+        for l in range(0, 11):
+            line = []
+            for c in range(0, 11):
+                if l == 0 and c == 0:
+                    line.append(' ' * (2 + self.col_space))
+                # LETTERS:
+                elif l == 0 and c != 0:
+                    line.append(self.text_color + self.columns[c - 1] + self.end_color + ' ' * self.col_space)
+                # NUMBERS:
+                elif l != 0 and c == 0:
+                    if l == 10:
+                        line.append(self.text_color + self.lines[l - 1] + self.end_color + ' ' * self.col_space)
+                    else:
+                        line.append(self.text_color + self.lines[l - 1] + self.end_color + ' ' * (1 + self.col_space))
+                # FIELD:
+                else:
+                    line.append(self.bg_color + self.bg_char + self.end_color + ' ' * self.col_space)
+            table.append(line)
+        return table
+
+    def render_field(self) -> None:
+        '''Render field.'''
+        for line in self.table:
+            print(''.join(line))
+        print('')
