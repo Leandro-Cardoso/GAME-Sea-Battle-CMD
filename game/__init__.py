47015fcb942369c7ebf8146627327f23ff150867
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-11-06 15:08:48 -0300

    Generate ships test

--- game/__init__.py
+++ game/__init__.py
@@ -61,20 +61,4 @@ class Game():
 seabattle = Game()
 seabattle.run()
 
-'''ships = {
-    5 : {'C' : 'carrier'},
-    4 : {'B' : 'battleship'},
-    3 : {'S' : 'submarine'},
-    2 : {'D' : 'destroyer'},
-    1 : {'' : ''}
-}
-
-system('cls')
-
-field = Field()
-field.render()
-
-score = Score()
-score.render()
-input('\033[34mATIRAR EM: \033[30;44m')
-print('\033[m')'''
+#input('\033[34mATIRAR EM: \033[30;44m')
--- game/field.py
+++ game/field.py
@@ -1,3 +1,5 @@
+from random import choice
+
 class Field():
     '''Create game field.'''
     def __init__(self, col_space:int = 2) -> None:
@@ -5,13 +7,60 @@ class Field():
         self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
         self.lines = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
         self.bg_char = '+'
+        self.ships = {
+            'carrier' : {
+                'code' : 'C',
+                'size' : 5
+            },
+            'battleship' : {
+                'code' : 'B',
+                'size' : 4
+            },
+            'submarine' : {
+                'code' : 'S',
+                'size' : 3
+            },
+            'destroyer' : {
+                'code' : 'D',
+                'size' : 2
+            },
+            'motorboat' : {
+                'code' : 'M',
+                'size' : 1
+            }
+        }
         self.bg_color = '\033[34m'
         self.text_color = '\033[33m'
+        self.ship_color = '\033[30m'
         self.table = self.create_table()
+
+    def is_field(self, position:str) -> bool:
+        '''Is field?'''
+        pass
+
+    def is_ship(self, position:str) -> bool:
+        '''Is ship?'''
+        pass
 
     def generate_ships(self) -> None:
         '''Generate ships'''
-        pass
+        # SHIP TYPE:
+        for i in range(list(self.ships)):
+            ship = self.ships[i]
+            # SHIP NUMBER:
+            n_ship = i + 1
+            for n in range(n_ship):
+                # SET POSITION:
+                is_set_ship_position = False
+                while not is_set_ship_position:
+                    position = choice(self.columns) + choice(self.lines)
+                    orientation = choice(['vertical', 'horizontal'])
+                    for j in range(ship['size']):
+                        # VERIFICAR SE CABE O NAVIO (OPERAÇÕES COM CONDICIONAIS)...
+                        pass
+                for j in range(ship['size']):
+                    # SETAR POSIÇÕES...
+                    pass
 
     def create_table(self) -> list:
         '''Create table.'''
