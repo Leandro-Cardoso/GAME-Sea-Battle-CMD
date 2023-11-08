8c3f15e2edf10227d5ce917f1af6687c16aa940e
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-11-08 11:55:32 -0300

    Shot string treatment

--- game/__init__.py
+++ game/__init__.py
@@ -9,6 +9,7 @@ from config import GAME_TITLE, GAME_DESCRIPTION
 class Game():
     '''Create game file.'''
     def __init__(self) -> None:
+        self.is_running = False
         self.player = Player()
         self.bot = Player(name = 'Bot', is_bot = True)
         self.score = Score()
@@ -67,6 +68,7 @@ class Game():
             print(self.text_color + '=' * 69)
             print(' 1 - Iniciar jogo')
             print(' 2 - Reposicionar navios')
+            print(' 3 - Sair do jogo')
             print('=' * 69)
             # RENDER ERROR:
             if error != '':
@@ -76,6 +78,7 @@ class Game():
             # VERIFY OPTION:
             option = input('\033[34mDIGITE O NUMERO DA OPÇÃO: \033[30;44m')
             print('\033[m')
+            option = option.replace(' ', '')
             if option == '1':
                 is_selected = True
                 error = ''
@@ -83,6 +86,8 @@ class Game():
                 self.player.ships_map.table = self.player.ships_map.create_table()
                 self.player.ships_map.generate_ships()
                 error = ''
+            elif option == '3':
+                self.is_running = False
             else:
                 error = 'Digite apenas o numero da opção escolhida.'
 
@@ -91,12 +96,9 @@ class Game():
         for i, line in enumerate(self.player.ships_map.table):
             print(''.join(line) + self.text_color + '|' + (' ' * 2) + ''.join(self.player.hits_map.table[i]))
 
-    def quit(self) -> None:
-        '''Quit game.'''
-        self.is_running = False
-
     def run(self) -> None:
         '''Run game.'''
+        self.is_running = True
         # CREATE PLAYER:
         self.player = Player()
         self.player.hits_map = Field()
@@ -113,19 +115,39 @@ class Game():
         self.bot.ships_map.generate_ships()
         # CREATE SCORE BOARD:
         self.score = Score(player_name = self.player.name, bot_name = self.bot.name)
-        # Enquanto o jogo não terminar:
-        # Variar entre turno do player e do bot.
-        # Tela de vitória.
-        # Tela de derrota.
+        # START MATCH:
+        error = ''
+        while self.is_running:
+            # RENDER:
+            self.render_title()
+            self.render_maps()
+            self.score.render()
+            # RENDER ERROR:
+            if error != '':
+                error = self.error_color + 'ERRO: ' + error
+                print(f'{error:^69}')
+            # SHOT:
+            shot = input('\033[34mDIGITE O ALVO DO DISPARO: \033[30;44m')
+            print('\033[m')
+            shot = shot.upper()
+            shot = shot.replace(' ', '')
+            if shot[0] in self.player.hits_map.lines and shot[1] in self.player.hits_map.columns:
+                shot = shot[1] + shot[0]
+            if shot[0] in self.player.hits_map.columns and shot[1] in self.player.hits_map.lines:
+                print(shot)
+                self.is_running = False
+            else:
+                error = 'Responda apenas com uma letra e um numero.'
+            # Realizar disparo.
+            # Verificar vitória ou derrota.
+            # Variar entre turno do player e do bot.
+            # Tela de vitória.
+            # Tela de derrota (pegar som de derrota).
+            # Continuar ou não.
 
-        # Adicionar comando para sair do jogo.
         # Adicionar efeitos sonoros.
         # Adicionar trilha sonora (função asinc).
 
-        # TESTS: (temp)
-        self.render_title()
-        self.render_maps()
-        self.score.render()
         # RESET COLORS:
         print('\033[m')
 
